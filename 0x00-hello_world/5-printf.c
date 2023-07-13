9a17006ce0b12a58d809c81897f04f143f01101b
Eleni07 <biruktawitkinde@gmail.com>
2023-07-13 15:06:38 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,9 +2,10 @@
 /**
  * main - a programm that uses printf function
  * Return: 0 (Success)
-*/
+ */
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n")
 return (0);
-} 
+}
+
